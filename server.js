d843aa0524e76c8eaaf4988d2c2c6466483766ce
KevinCuchon <131375398+KevinCuchon@users.noreply.github.com>
2024-10-03 18:08:13 -0500

    First Change

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -13,7 +14,16 @@ const static = require("./routes/static")
 /* ***********************
  * Routes
  *************************/
+
+app.use(require("./routes/static.js"))
+app.get("/", function(_req, res){
+  res.render("index", {title:"Home"})
+})
+
 app.use(static)
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Local Server Information
