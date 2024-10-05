9c5af15099f515081303bc6b4476024ca917cf2a
KevinCuchon <131375398+KevinCuchon@users.noreply.github.com>
2024-10-04 20:59:13 -0500

    Finish Commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
+* This server.js file is the primary file of the 
+* application. It is used to control the project.
+*******************************************/
 /* ***********************
- * Require Statements
- *************************/
+* Require Statements
+*************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -12,29 +12,31 @@ const app = express()
 const static = require("./routes/static")
 
 /* ***********************
- * Routes
- *************************/
-
-app.use(require("./routes/static.js"))
-app.get("/", function(_req, res){
-  res.render("index", {title:"Home"})
-})
-
-app.use(static)
+* View Engine and Templates
+*************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
+* Routes
+*************************/
+app.use(static)
+
+app.get("/", function(req, res){
+  res.render("index", {title:"Home"})
+})
+
+/* ***********************
+* Local Server Information
+* Values from .env (environment) file
+*************************/
 const port = process.env.PORT
 const host = process.env.HOST
 
 /* ***********************
- * Log statement to confirm server operation
- *************************/
+* Log statement to confirm server operation
+*************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
